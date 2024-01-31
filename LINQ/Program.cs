20cff7733e49735da9559f6d4988387aa639fcde
John Franzese <johnfranzese21@gmail.com>
2024-01-31 12:00:48 -0600

    LINQ Exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create the list of video games type string
+            List<string> videoGames = new List<string>
+        {
+            "NBA 2K",
+            "Super Mario",
+            "World of Warcraft",
+            "Red Dead Redemption",
+            "Fortnite",
+            "Minecraft",
+            "Overwatch",
+            "COD",
+            "Halo",
+            "SSS Tricky"
+        };
+
+            // Order the list by the length of the game name using LINQ
+            var orderedGames = videoGames.OrderBy(game => game.Length);
+
+            // Print the list of video game names
+            Console.WriteLine("An Ordered List of Video Game Names:");
+            foreach (string game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
+
         }
     }
 }
